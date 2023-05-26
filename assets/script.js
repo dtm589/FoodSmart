6b7289d9778f2127eb259a74ec5be713fa8a1acd
Derek Marchese <derekmarchese1@gmail.com>
2023-05-25 20:19:00 -0400

    fixed the if statement and added function for local storage

--- assets/script.js
+++ assets/script.js
@@ -8,11 +8,9 @@ $("#search-form").on("submit", function (event) {
 
     //get UPC
     let UPC = $("#UPC-input").val();
-
-    //ensure something is entered and it is 10 digits
-    if (UPC === "" || UPC == null || UPC.legnth > 10 || UPC.legnth < 10) {
+    //ensure something is entered and it is 10 digits, AND ONLY DIGITS
+    if (UPC === "" || UPC.length !== 12) {
         alert("We need to change this to a notification in Bulma");
-        event.preventDefault();
     } else {
         //add to search history list and display item
         currentUPCsearch(UPC);
@@ -22,39 +20,70 @@ $("#search-form").on("submit", function (event) {
     $("#UPC-input").val("");
 });
 
+
+//make a list of searched cities
+let searchHistoryList = function (itemName) {
+
+    //no duplicate 
+    $('.past-search:contains("' + UPC + '")').remove();
+
+    //create entry with item name
+    let serachHistoryEntry = $("<h5>");
+    serachHistoryEntry.text(itemName);
+
+    //create container for each entry
+    let searchEntryContainer = $("<div>")
+
+    //append entry to container, and container to search history container
+    searchEntryContainer.append(serachHistoryEntry);
+    let searchHistoryContainerEl = $("# "); //NEED TO ADD THE ID OF THE CONTAINER IN THE HTML
+    searchHistoryContainerEl.append(searchEntryContainer);
+
+    if (savedSearches.length > 0) {
+        let previousSavedSearches = localStorage.getItem("savedSearches");
+        savedSearches = JSON.parse(previousSavedSearches);
+    }
+
+    //add searched item to array of searches
+    savedSearches.push(itemName);
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+}
+
+//load saved history entries into the container
+
 // Will display the current item
 let currentUPCsearch = function (UPC) {
     //get data from API
     fetch(`https://api.spoonacular.com/food/products/upc/${UPC}?apiKey=${apiKey}`)
-        .then(function (responce) {
-            return responce.json();
+        .then(function (response) {
+            return response.json();
         })
-        .then(function (responce) {
-            //searchHistoryList(itemName)
-
-            let currentItemContainer = $("#display-container");
+        .then(function (response) {
 
             //add item name, img, badges, fat, protien, carbs, and calories
             let currentName = $("#item-name");
-            currentName.text(responce.title);
+            currentName.text(response.title);
+            let itemName = currentName.val();
 
             let currentImg = $("#item-img");
-            let currentImgSrc = responce.images[1];
+            let currentImgSrc = response.images[1];
             currentImg.attr("src", `${currentImgSrc}`)
 
             let currentBadges = $("#item-badges");
-            currentBadges.text(responce.badges + responce.importantBadges);
+            currentBadges.text(response.badges + response.importantBadges);
 
             let currentFat = $("#item-fat");
-            currentFat.text("Total Fat: " + responce.nutrition.fat);
+            currentFat.text("Total Fat: " + response.nutrition.fat);
 
             let currentProtein = $("#item-protein");
-            currentProtein.text("Total Protein: " + responce.nutrition.protein);
+            currentProtein.text("Total Protein: " + response.nutrition.protein);
 
             let currentCarbs = $("#item-carbs");
-            currentCarbs.text("Total Carbs: " + responce.nutrition.carbs);
+            currentCarbs.text("Total Carbs: " + response.nutrition.carbs);
 
             let currentCalories = $("#item-calories");
-            currentCalories.text("Total Calories: " + responce.nutrition.calories);
+            currentCalories.text("Total Calories: " + response.nutrition.calories);
+
+            searchHistoryList(itemName)
         })
 };
