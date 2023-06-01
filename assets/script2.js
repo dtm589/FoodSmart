6a2a7993ab2143bcc3d3c680070cc0da0a924116
Derek Marchese <derekmarchese1@gmail.com>
2023-05-31 20:39:04 -0400

    second page javascript functions

--- assets/script2.js
+++ assets/script2.js
@@ -2,11 +2,11 @@
 let apiKey = '78cfe3cd7a5b4fcba48601e913f2d073';
 
 // add event listener for sumbit button
-$("#recipeSelect").on("submit", function (event) {
+$("#diets").on("submit", function (event) {
     event.preventDefault();
 
     //get the value entered in the drop box
-    let dietChoice = $("#userSelect").val(); 
+    let dietChoice = $("#dietSelect").val(); 
 
     //call the function to make the api fetch
     currentdietSearch(dietChoice);
@@ -23,40 +23,40 @@ let currentdietSearch = function (dietChoice) {
         .then(function (response) {
 
             //recipe 1 info
-            let recipeName1 = $("#recipe1Name");
+            let recipeName1 = $("#recipeName1");
             recipeName1.text(response.meals[0].title)
 
-            let recipeImage1 = $("#recipe1Img");
+            let recipeImage1 = $("#recipeImage1");
             let recipeImage1ID = response.meals[0].id;
             recipeImage1.attr("src", `https://spoonacular.com/recipeImages/${recipeImage1ID}-312x231.jpg`);
 
-            let recipeURL1 = $("#recipe1URL");
+            let recipeURL1 = $("#recipeLink1");
             let recipeURL1Src = response.meals[0].sourceUrl;
             recipeURL1.attr("href", `${recipeURL1Src}`);
             recipeURL1.text(recipeURL1Src);
 
             //recipe 2 info
-            let recipeName2 = $("#recipe2Name");
+            let recipeName2 = $("#recipeName2");
             recipeName2.text(response.meals[1].title)
 
-            let recipeImage2 = $("#recipe2Img");
+            let recipeImage2 = $("#recipeImage2");
             let recipeImage2ID = response.meals[1].id;
             recipeImage2.attr("src", `https://spoonacular.com/recipeImages/${recipeImage2ID}-312x231.jpg`);
 
-            let recipeURL2 = $("#recipe2URL");
+            let recipeURL2 = $("#recipeLink2");
             let recipeURL2Src = response.meals[1].sourceUrl;
             recipeURL2.attr("href", `${recipeURL2Src}`);
             recipeURL2.text(recipeURL2Src)
 
             //recipe 3 info
-            let recipeName3 = $("#recipe3Name");
+            let recipeName3 = $("#recipeName3");
             recipeName3.text(response.meals[2].title)
 
-            let recipeImage3 = $("#recipe3Img");
+            let recipeImage3 = $("#recipeImage3");
             let recipeImage3ID = response.meals[2].id;
             recipeImage3.attr("src", `https://spoonacular.com/recipeImages/${recipeImage3ID}-312x231.jpg`);
 
-            let recipeURL3 = $("#recipe3URL");
+            let recipeURL3 = $("#recipeLink3");
             let recipeURL3Src = response.meals[2].sourceUrl;
             recipeURL3.attr("href", `${recipeURL3Src}`);
             recipeURL3.text(recipeURL3Src);
